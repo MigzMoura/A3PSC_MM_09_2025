f249f35b73a130ee820589db97eae0146475599e
Miguel Moura <moura.23.miguel@gmail.com>
2025-09-05 17:13:56 -0300

    tst_main_painel

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
+        //System.out.println("-> Cadastro de Usuário <-")
+        //System.out.print("Nome: ")
+        //System.out.print("Login: ")
     }
 }
